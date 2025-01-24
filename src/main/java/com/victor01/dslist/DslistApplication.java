9de120e48d51db09f68b076aaf491de65e257924
Paulo Victor <paulovictor2536@gmail.com>
2025-01-24 17:05:13 -0300

    Project created

---
+++ src/main/java/com/victor01/dslist/DslistApplication.java
@@ -0,0 +1,13 @@
+package com.victor01.dslist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DslistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DslistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/victor01/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victor01.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
