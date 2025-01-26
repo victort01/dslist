03257b6d40ae4e098fc067c1ea7bbcae14952baf
Paulo Victor <paulovictor2536@gmail.com>
2025-01-26 18:15:41 -0300

    SQL, projection, GET games by list

---
+++ src/main/java/com/victor01/dslist/projections/GameMinProjection.java
@@ -0,0 +1,11 @@
+package com.victor01.dslist.projections;
+
+public interface GameMinProjection {
+
+	Long getId();
+	String getTitle();
+	Integer getYear();
+	String getImgUrl();
+	String getShortDescription();
+	Integer getPositon();
+}
--- src/main/java/com/victor01/dslist/services/GameService.java
+++ src/main/java/com/victor01/dslist/services/GameService.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.victor01.dslist.dto.GameDTO;
 import com.victor01.dslist.dto.GameMinDTO;
 import com.victor01.dslist.entities.Game;
+import com.victor01.dslist.projections.GameMinProjection;
 import com.victor01.dslist.repositories.GameRepository;
 
 @Service
@@ -29,4 +30,10 @@ public class GameService {
 		return result.stream().map(x -> new GameMinDTO(x)).toList();
 	}
 	
+	
+	@Transactional(readOnly = true)
+	public List<GameMinDTO> findByList(Long listId){
+		List<GameMinProjection> result = gameRepository.searchByList(listId);
+		return result.stream().map(x -> new GameMinDTO(x)).toList();
+	}
 }
